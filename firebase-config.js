27e5e51fabad1bbe777c4ff6e94bd9a45bf83603
RzWojtek <116317334+RzWojtek@users.noreply.github.com>
2026-05-25 13:57:02 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// =====================================================
+// firebase-config.js
+// UZUPEŁNIJ dane ze swojego projektu Firebase!
+// =====================================================
+const firebaseConfig = {
+  apiKey:            "AIzaSyDXGwtyjQF7phmStS1Scrj4OGrfY79dVSg",
+  authDomain:        "notefold-79731.firebaseapp.com",
+  projectId:         "notefold-79731",
+  storageBucket:     "notefold-79731.firebasestorage.app",
+  messagingSenderId: "208993160504",
+  appId:             "1:208993160504:web:e9d3fe925ed3617063c5cd"
+};
+
+firebase.initializeApp(firebaseConfig);
